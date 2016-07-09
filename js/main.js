c5e15c245cf03fa5a4af3d1198b46defaf2b1dfc
Liloka <elizabeth.chesters@gmail.com>
2016-07-09 23:10:50 +0100

    Added scroll through gallery for photography

---
+++ js/main.js
@@ -0,0 +1,30 @@
+ jQuery(document).ready(function($) {
+ 
+    $(".scroll a, .navbar-brand, .gototop").click(function(event){   
+    event.preventDefault();
+    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 600,'swing');
+    $(".scroll li").removeClass('active');
+    $(this).parents('li').toggleClass('active');
+    });
+    });
+
+var wow = new WOW(
+  {
+    boxClass:     'wowload', 
+    animateClass: 'animated',
+    offset:       0,         
+    mobile:       true,      
+    live:         true       
+  }
+);
+wow.init();
+
+$('.carousel').swipe( {
+     swipeLeft: function() {
+         $(this).carousel('next');
+     },
+     swipeRight: function() {
+         $(this).carousel('prev');
+     },
+     allowPageScroll: 'vertical'
+ });
